3ff565806bb60e2b9adced6fa3aa6637b8fcfb62
Pulkiteleven <92856883+Pulkiteleven@users.noreply.github.com>
2023-01-28 16:01:55 +0530

    New Commit on 28 jan

---
+++ lib/Usefull/Buttons.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+
+import 'Colors.dart';
+
+double borderRadius = 10.0;
+
+class btnsss extends StatelessWidget {
+  VoidCallback callback;
+  String title;
+  Color main;
+  Color text;
+
+  btnsss(this.title,this.callback,this.main,this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(3, 0, 3, 0),
+        child: mainText(
+            title, text, 13.0, FontWeight.normal,1),
+      ),
+      style: ButtonStyle(
+          foregroundColor:
+          MaterialStateProperty.all<Color>(
+              main),
+          backgroundColor:
+          MaterialStateProperty.all<Color>(main),
+          shape: MaterialStateProperty.all<
+              RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                  borderRadius:
+                  BorderRadius.circular(borderRadius),
+                  side: BorderSide(color: main)))),
+      onPressed: callback,
+    );
+  }
+}
+
+class borderbtnsss extends StatelessWidget {
+  VoidCallback callback;
+  String title;
+  Color text;
+
+  borderbtnsss(this.title,this.callback,this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(3, 0, 3, 0),
+        child: mainText(
+            title, text, 13.0, FontWeight.normal,1),
+      ),
+      style: ButtonStyle(
+          foregroundColor:
+          MaterialStateProperty.all<Color>(
+              transparent_overlay),
+          backgroundColor:
+          MaterialStateProperty.all<Color>(transparent_overlay),
+          shape: MaterialStateProperty.all<
+              RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                  borderRadius:
+                  BorderRadius.circular(borderRadius),
+                  side: BorderSide(color: text,width: 2.0)))),
+      onPressed: callback,
+    );
+  }
+}
+
+class fullbtnsss extends StatelessWidget {
+  VoidCallback callback;
+  String title;
+  Color main;
+  Color text;
+
+  fullbtnsss(this.title,this.callback,this.main,this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(3, 0, 3, 0),
+        child: Row(
+          children: [
+            Spacer(),
+            mainText(
+                title, text, 13.0, FontWeight.normal,1),
+            Spacer()
+          ],
+        ),
+      ),
+      style: ButtonStyle(
+          foregroundColor:
+          MaterialStateProperty.all<Color>(
+              main),
+          backgroundColor:
+          MaterialStateProperty.all<Color>(main),
+          shape: MaterialStateProperty.all<
+              RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                  borderRadius:
+                  BorderRadius.circular(borderRadius),
+                  side: BorderSide(color: main)))),
+      onPressed: callback,
+    );
+  }
+}
+
+class borderfullbtnsss extends StatelessWidget {
+  VoidCallback callback;
+  String title;
+  Color text;
+
+  borderfullbtnsss(this.title,this.callback,this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(3, 0, 3, 0),
+        child: Row(
+          children: [
+            Spacer(),
+            mainText(
+                title, text, 13.0, FontWeight.normal,1),
+            Spacer()
+          ],
+        ),
+      ),
+      style: ButtonStyle(
+          foregroundColor:
+          MaterialStateProperty.all<Color>(
+              transparent_overlay),
+          backgroundColor:
+          MaterialStateProperty.all<Color>(transparent_overlay),
+          shape: MaterialStateProperty.all<
+              RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                  borderRadius:
+                  BorderRadius.circular(borderRadius),
+                  side: BorderSide(color: transparent_overlay)))),
+      onPressed: callback,
+    );
+  }
+}
+
+
+
+
+
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
+import 'package:event_app/Usefull/Colors.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(MaterialApp());
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(
+    home: Splash(),
+  ));
+}
+
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: mainColor,
+        body: Column(
+          mainAxisSize:MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Spacer(),
+                mainText("Avent", bgColor, 30.0, FontWeight.bold, 1),
+                Spacer(),
+
+              ],
+            ),
+            mainText("ASPER", bgLight, 10.0, FontWeight.normal, 1),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 
