d355472babd802b9a3e569c79fcbcc4aeaf99c77
Dhiraj320 <dhirajkumawat320@gmail.com>
2023-03-04 15:22:06 +0530

    Nav Screen1

---
+++ lib/NavDrawer/nav_screen/settings.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation:0,
+          backgroundColor:Colors.transparent,
+      ),
+
+    );
+  }
+}
